21aacc8c87ffb44bd8ea68a5369eea29cce80d47
edesgree@version10.ca <Amon666!>
2016-12-01 10:05:01 -0500

    nouvelles pages2

--- gulpFile.js
+++ gulpFile.js
@@ -38,7 +38,7 @@ gulp.task('less', function(){
         .pipe(concat('styles.min.css'))
         .pipe(less({
             modifyVars: {
-                path_bower:'~"../../../bower_components/"' 
+                path_bower:'~"../../../bower_components/"'
                 //path : '~"../../images/build/"' //modification du path des images en prod
             }
         }))
